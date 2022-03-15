89d75e6055694fd9b11448da3a0b68a9e562bfe1
Alejandro Ortiz <aleortbas@gmail.com>
2022-03-14 23:01:59 -0500

    login terminado y inicio pagina ventas

--- src/app/ferre/ferre.page.ts
+++ src/app/ferre/ferre.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ferre',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FerrePage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
+  go(){
+    this.router.navigate(['home']); 
+  }
   ngOnInit() {
   }
 
